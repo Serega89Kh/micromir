a9d67ad3b9c9b22fcdaf13f2c6433d8603a54b0f
Sergey Khramov <31696621+Serega89Kh@users.noreply.github.com>
2020-02-14 00:14:24 +0300

    Add files via upload

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Food : MonoBehaviour
+{
+    public static int CountClones = 1;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+        if (CountClones > 0)
+        {
+            CountClones--;
+            var randomPosition = new Vector3(Random.Range(-100, 100), 4, Random.Range(-100, 100));
+            var clone = Instantiate(gameObject, randomPosition, Quaternion.identity) as GameObject;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Destroy(gameObject);
+            CountClones++;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float reset = 5;
+    private float newtime;
+
+
+    private Color basicColor = Color.green;
+    private Color hoverColor = Color.red;
+    private Renderer renderer;
+
+    public float seeDistance = 10f;
+    public float speed;
+    private Transform target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newtime = Time.time;
+        rb = GetComponent<Rigidbody>();
+
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = basicColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = GameObject.FindWithTag("Food").transform;
+        reset -= Time.deltaTime;
+        if (reset > 0)
+        {     
+            transform.GetComponent<Renderer>().material.color = Color.Lerp(basicColor, hoverColor, (Time.time-newtime)/5);
+            if (Vector3.Distance(transform.position, target.transform.position) < seeDistance)
+            {
+                transform.LookAt(target.transform);
+                transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+            }
+        }
+
+        if (reset < 0)
+        {
+            transform.GetComponent<Renderer>().material.SetColor ("_Color", Color.black);
+            GetComponent<SphereCollider>().enabled = false;
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Food")
+        {
+            reset = 5;
+            newtime = Time.time;
+            print(newtime);
+            Instantiate(gameObject);
+        }
+    }
+}
